94262953c97d82d1efbe321b9b3cb20061ac4003
Roman Kabaev <r.kabaev@yadro.com>
2024-07-10 12:46:50 +0300

    A bit of refactoring and more explicit error handling

--- src/serialization.rs
+++ src/serialization.rs
@@ -1,4 +1,5 @@
 use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
+use std::cell::Ref;
 
 use pyo3::{pyfunction, types::PyDict, FromPyObject, IntoPy, PyAny, PyObject, PyResult, Python};
 
@@ -34,23 +35,28 @@ impl<'source> FromPyObject<'source> for TreeObject {
         let parent_key: Option<String> = obj
             .getattr("parent_key")
             .expect("Could not get parent key from python object")
-            .extract()?;
+            .extract()
+            .expect("Could not get PyResult");
         let pk_key: String = obj
             .getattr("pk_key")
             .expect("Could not_get pk key from python object")
-            .extract()?;
+            .extract()
+            .expect("Could not get String from PyResult");
         let mut instances = Vec::new();
         let py_instances: Vec<PyObject> = obj
             .getattr("instances")
-            .unwrap()
-            .extract()?;
+            .expect("Could not unwrap PyAny")
+            .extract()
+            .expect("Could not cast PyResult to Vec<PyObject>");
         for py_instance in py_instances {
-            let py_dict = py_instance.downcast::<PyDict>(obj.py())?;
+            let py_dict = py_instance.downcast::<PyDict>(obj.py())
+                .expect("Could not downcast to PyDict");
             let id = py_dict
                 .get_item(&pk_key)
-                .unwrap()
-                .unwrap()
-                .extract()?;
+                .expect("Could not retrieve PyAny")
+                .expect("Option was not provided")
+                .extract()
+                .expect("Could not extract PyResult");
             let mut parent_id = None;
             if let Some(parent_id_from_py) = py_dict
                 .get_item(&parent_key)
@@ -85,15 +91,16 @@ impl IntoPy<PyObject> for Parent {
 
 impl IntoPy<PyObject> for PythonInstance {
     fn into_py(self, py: Python) -> PyObject {
-        let dict = self.dict.downcast::<PyDict>(py).unwrap();
+        let dict = self.dict.downcast::<PyDict>(py)
+            .expect("Could not downcast PyDict");
         let children: Vec<PyObject> = self
             .children
             .iter()
             .map(|elem| elem.borrow().clone().into_py(py))
             .collect();
-        dict.set_item("children", children).unwrap();
+        dict.set_item("children", children).expect("Could not set key");
         for (key, value) in self.related_objects {
-            dict.set_item(key, value).unwrap()
+            dict.set_item(key, value).expect("Could not set key")
         }
         dict.into()
     }
@@ -110,8 +117,7 @@ pub fn serialize_tree(
     let mut root_refs: Vec<PythonInstanceRef> = Vec::new();
     for mut dict in data_set_object.instances {
         for prefetch_object in &prefetch_objects {
-            dict.related_objects
-                .insert(prefetch_object.group_key.clone(), vec![]);
+            dict.related_objects.insert(prefetch_object.group_key.clone(), vec![]);
         }
         dict_map.insert(dict.id, Rc::new(RefCell::new(dict)));
     }
@@ -121,39 +127,47 @@ pub fn serialize_tree(
     }
     for dict in dict_map.values() {
         let borrowed_dict = dict.borrow();
+
         if let Some(parent_id) = borrowed_dict.parent_id {
             if let Some(parent) = dict_map.get(&parent_id) {
-                let py_dict = borrowed_dict
-                    .dict
-                    .downcast::<PyDict>(py)
-                    .expect("Could not downcast to PyDict");
-                let mut borrowed_parent = parent.borrow_mut();
-                borrowed_parent.children.push(Rc::clone(dict));
-                let name: String = borrowed_parent
-                    .dict
-                    .downcast::<PyDict>(py)
-                    .expect("Could not cast PyAny to PyDict")
-                    .get_item("name")
-                    .expect("name was not found in PyDict")
-                    .unwrap()
-                    .extract()
-                    .expect("Could not extract String from PyObject");
-                py_dict
-                    .set_item(
-                        "parent",
-                        Parent {
-                            id: parent_id,
-                            name,
-                        }
-                        .into_py(py),
-                    )
-                    .expect("Could not set item in dict");
+                map_parent(parent, parent_id, dict, borrowed_dict, py)
             }
         } else {
             root_refs.push(Rc::clone(dict))
         }
     }
     root_refs.iter().map(|root| root.borrow().clone()).collect()
+}
+
+fn map_parent(
+    parent: &Rc<RefCell<PythonInstance>>,
+    parent_id: u32,
+    dict: &Rc<RefCell<PythonInstance>>,
+    borrowed_dict: Ref<PythonInstance>,
+    py: Python,
+) {
+    let py_dict = borrowed_dict
+        .dict
+        .downcast::<PyDict>(py)
+        .expect("Could not downcast PyObject to PyDict");
+    let mut borrowed_parent = parent.borrow_mut();
+    borrowed_parent.children.push(Rc::clone(dict));
+    let name: String = borrowed_parent
+        .dict
+        .downcast::<PyDict>(py)
+        .expect("Could not cast PyAny to PyDict")
+        .get_item("name")
+        .expect("name was not found in PyDict")
+        .expect("unwrapping to PyAny failed")
+        .extract()
+        .expect("Could not extract String from PyObject");
+    py_dict.set_item(
+        "parent",
+        Parent {
+            id: parent_id,
+            name,
+        }.into_py(py),
+    ).expect("Could not set item in dict");
 }
 
 fn add_related_objects(
@@ -172,7 +186,7 @@ fn add_related_objects(
                 .expect("fk was not found")
                 .extract()
                 .expect("Could not convert PyObject to u32");
-            let relation_map = &mut dict_map.get_mut(&fk).unwrap().borrow_mut().related_objects;
+            let relation_map = &mut dict_map.get_mut(&fk).expect("Could not get map").borrow_mut().related_objects;
             if let Some(related_objects) = relation_map.get_mut(&prefetch_object.group_key) {
                 related_objects.push(instance);
             }
